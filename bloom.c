4ce3d184ebb295b309fc84cf9d8e1d6964ca7340
David Michael Barr <b@rr-dav.id.au>
2012-09-07 12:45:12 +1000

    bloom: tighten derefs in git_uniq
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- bloom.c
+++ bloom.c
@@ -40,16 +40,16 @@ static inline int git_bloom_test_sha1(const struct git_bloom *bloom, const char 
 void git_uniq(struct git_bloom *bloom, const char *sha1, const uint32_t *size, uint32_t n)
 {
 	while (n--) {
-		const int il2 = *size ? 31 - __builtin_clz(*size) : 0;
+		const uint32_t sz = *size++;
+		const int il2 = sz ? 31 - __builtin_clz(sz) : 0;
 		if (!git_bloom_test_sha1(bloom, sha1)) {
 			git_bloom_set_sha1(bloom, sha1);
 			bloom->count[il2]++;
-			bloom->size[il2] += *size;
+			bloom->size[il2] += sz;
 		}
 		bloom->objects[il2]++;
-		bloom->total[il2] += *size;
+		bloom->total[il2] += sz;
 		sha1 += 20;
-		size++;
 	}
 }
 
